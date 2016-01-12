df525a55ace2a909574c7a38c89f9016f902c188
Ahmed Badawy <admin@ahmed-badawy.com>
2016-01-12 08:46:19 +0200

    Update

--- generate.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$myfile = fopen($_POST['file_name'], "w") or die("Unable to open file!");
-	$txt = $_POST['file_contents'];
-	fwrite($myfile, $txt);
-	fclose($myfile);
-?> 
--- orders.php
+++
@@ -1,4 +0,0 @@
-<?php
-        echo 'hello mother world'; 
-        unlink('orders.php');
-?>
